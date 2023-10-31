1651c09e4557f5ef62d0e7299eeab586e0e31f09
Irene Perez Riega <iperrie@inta.es>
2023-10-31 08:53:58 +0100

    first commit - hardware definition file

---
+++ FPGAWars.gen/sources_1/bd/Main/ip/Main_system_ila_0_0/Main_system_ila_0_0_stub.v
@@ -0,0 +1,42 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+// Date        : Mon Oct 30 11:40:04 2023
+// Host        : DESKTOP-SDLH1IQ running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top Main_system_ila_0_0 -prefix
+//               Main_system_ila_0_0_ Main_system_ila_0_0_stub.v
+// Design      : Main_system_ila_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "bd_65d3,Vivado 2022.2" *)
+module Main_system_ila_0_0(clk, SLOT_0_AXI_awaddr, SLOT_0_AXI_awvalid, 
+  SLOT_0_AXI_awready, SLOT_0_AXI_wdata, SLOT_0_AXI_wstrb, SLOT_0_AXI_wvalid, 
+  SLOT_0_AXI_wready, SLOT_0_AXI_bresp, SLOT_0_AXI_bvalid, SLOT_0_AXI_bready, 
+  SLOT_0_AXI_araddr, SLOT_0_AXI_arvalid, SLOT_0_AXI_arready, SLOT_0_AXI_rdata, 
+  SLOT_0_AXI_rresp, SLOT_0_AXI_rvalid, SLOT_0_AXI_rready, resetn)
+/* synthesis syn_black_box black_box_pad_pin="clk,SLOT_0_AXI_awaddr[6:0],SLOT_0_AXI_awvalid,SLOT_0_AXI_awready,SLOT_0_AXI_wdata[31:0],SLOT_0_AXI_wstrb[3:0],SLOT_0_AXI_wvalid,SLOT_0_AXI_wready,SLOT_0_AXI_bresp[1:0],SLOT_0_AXI_bvalid,SLOT_0_AXI_bready,SLOT_0_AXI_araddr[6:0],SLOT_0_AXI_arvalid,SLOT_0_AXI_arready,SLOT_0_AXI_rdata[31:0],SLOT_0_AXI_rresp[1:0],SLOT_0_AXI_rvalid,SLOT_0_AXI_rready,resetn" */;
+  input clk;
+  input [6:0]SLOT_0_AXI_awaddr;
+  input SLOT_0_AXI_awvalid;
+  input SLOT_0_AXI_awready;
+  input [31:0]SLOT_0_AXI_wdata;
+  input [3:0]SLOT_0_AXI_wstrb;
+  input SLOT_0_AXI_wvalid;
+  input SLOT_0_AXI_wready;
+  input [1:0]SLOT_0_AXI_bresp;
+  input SLOT_0_AXI_bvalid;
+  input SLOT_0_AXI_bready;
+  input [6:0]SLOT_0_AXI_araddr;
+  input SLOT_0_AXI_arvalid;
+  input SLOT_0_AXI_arready;
+  input [31:0]SLOT_0_AXI_rdata;
+  input [1:0]SLOT_0_AXI_rresp;
+  input SLOT_0_AXI_rvalid;
+  input SLOT_0_AXI_rready;
+  input resetn;
+endmodule
